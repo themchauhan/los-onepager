2165bc9188ffc48443720ea25abb6e63312b3bca
themchauhan <manish@192.168.1.3>
2026-08-26 20:04:35 +0530

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
 import './index.css'
 
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
